338280d15de367317845bfa00980f44e7939fdda
ThiagoP11 <thiagopnascimento11@gmail.com>
2024-07-24 14:35:05 -0300

    Novo Projeto

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+        //Thiago
         Categoria celulares = new Categoria("CELULARES");
 
         Produto celular = new Produto("Xiaomi Redmi","Muito Legal", new BigDecimal("800"), celulares);
